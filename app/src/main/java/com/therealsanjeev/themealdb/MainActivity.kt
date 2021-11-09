3088cc4f3eae7e7e9ab284619000e3ecaea2c3c6
sanjeev <therealsanjeev0@gmail.com>
2021-11-09 19:27:14 +0530

    search layout implemented

--- app/src/main/java/com/therealsanjeev/themealdb/MainActivity.kt
+++ app/src/main/java/com/therealsanjeev/themealdb/MainActivity.kt
@@ -2,10 +2,20 @@ package com.therealsanjeev.themealdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.therealsanjeev.themealdb.fragment.view.SearchMealFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setCurrentFragment(SearchMealFragment())
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, fragment)
+            commit()
+        }
 }
--- app/src/main/java/com/therealsanjeev/themealdb/fragment/view/SearchMealFragment.kt
+++ app/src/main/java/com/therealsanjeev/themealdb/fragment/view/SearchMealFragment.kt
@@ -14,6 +14,6 @@ class SearchMealFragment() : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_meal, container, false)
+        return inflater.inflate(R.layout.fragment_search_meal, container, false)
     }
 }
